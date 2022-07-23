ef8b0656381b99d5ec565c0274862dabbf20e2a3
Ryan Bartusek <bar2sek@outlook.com>
2022-07-23 13:55:08 -0500

    have to resize disk manually in GUI

--- hypervisor/ubuntu-init.sh
+++ hypervisor/ubuntu-init.sh
@@ -42,7 +42,7 @@ qm create ${ID} --memory ${MEMORY} --name ${VM_NAME} --net0 virtio,bridge=${NETW
 qm importdisk ${ID} /mnt/pve/cephfs/template/iso/focal-server-cloudimg-amd64.img ceph_pool
 
 qm set ${ID} --scsihw virtio-scsi-pci \
- --scsi0 ceph_pool:vm-${ID}-disk-0,size=32G \
+ --scsi0 ceph_pool:vm-${ID}-disk-0 \
  --ide2 ceph_pool:cloudinit \
  --boot c --bootdisk scsi0 \
  --serial0 socket --vga serial0
